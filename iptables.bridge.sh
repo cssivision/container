1ea30cc214ffa9b7c840ece648cbf08a05579ac6
cssivision <cssivision@gmail.com>
2018-01-17 21:54:58 +0800

    iptables rule

---
+++ iptables.bridge.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+VETH="container0"
+VETH_ADDR="10.88.37.1"
+
+iptables -P FORWARD DROP
+iptables -F FORWARD
+
+# Flush nat rules.
+iptables -t nat -F
+
+# Enable masquerading of 10.200.1.0.
+iptables -t nat -A POSTROUTING -s ${VETH_ADDR}/24 -o eth0 -j MASQUERADE
+
+iptables -A FORWARD -i eth0 -o ${VETH} -j ACCEPT
+iptables -A FORWARD -o eth0 -i ${VETH} -j ACCEPT
